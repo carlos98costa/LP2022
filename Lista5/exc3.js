bc143ba2c9186fa62fed19640e83cc265394e5a3
carlos98costa <46066793+carlos98costa@users.noreply.github.com>
2022-10-24 11:11:51 -0300

    update

--- Lista5/exc3.js
+++ Lista5/exc3.js
@@ -1,15 +1,23 @@
 function exc3() {
     let pessoas = []
-    let mediaIdades = 0
+    let somaIdades = 0
+    let mediaIdades1 = 0
+    let count1 = 0
     for (i = 1; i<3; i++) {
         dados = {
-            sexo: prompt(`Informe o sexo da ${i}ª pessoa:\n M. Masculino \n F. Feminino `),
-            altura: Number(prompt(`Informe a altura da ${i}ª pessoa:}`)),
-            peso: Number(prompt(`Informe o peso da ${i}ª pessoa:}`)),
-            olhos: prompt(`Informe a cor dos olhos da ${i}ª pessoa:\n A. Azuis \n V. Verdes \n C. Castanhos `)
+            sexo: (prompt(`Informe o sexo da ${i}ª pessoa:\n M. Masculino \n F. Feminino `)),
+            altura: Number(prompt(`Informe a altura da ${i}ª pessoa:`)),
+            idade: Number(prompt(`Informe a idade da ${i}ª pessoa:`)),
+            olhos: (prompt(`Informe a cor dos olhos da ${i}ª pessoa:\n A. Azuis \n V. Verdes \n C. Castanhos `))
         }
         pessoas.push(dados)
+        if(dados.olhos == 'C' || dados.olhos == 'c' && dados.altura > 1.6) {
+            count1 = count1 + 1
+            somaIdades1 = somaIdades1 + dados.idade
+        }
+        somaIdades = somaIdades + dados.idade
         
     }
     console.log(pessoas)
+    console.log(count1)
 }
